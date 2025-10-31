6e3d36612993f1570f807a612db1abff4ebca3fc
forestydude <93602327+forestydude@users.noreply.github.com>
2025-10-30 23:07:03 -0400

    Updated gravity vector determination
    Determine the gravity vector based on acceleration near 9.8^2. Needs some smoothing to prevent gradual gravity vector drift. This is used to prevent user from rotating the stick and making the drum hit at the wrong angle.
    Suggestions are to use quaternions to calculate rotation of vector instead.

--- ESP_NOW_Accelerometer_Client/ESP_NOW_Accelerometer_Client.ino
+++ ESP_NOW_Accelerometer_Client/ESP_NOW_Accelerometer_Client.ino
@@ -15,7 +15,7 @@ uint8_t broadcastAddress[] = {0xcc,0x8d,0xa2,0xed,0xce,0xb0};
 typedef struct midi_message {
     int id; // must be unique for each sender board
     int note;
-    int velocity;
+    float velocity;
 } midi_message;
 
 midi_message MIDIData;
@@ -25,10 +25,16 @@ esp_now_peer_info_t peerInfo;
 
 // callback when data is sent
 void OnDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  //Serial.print("\r\nLast Packet Send Status:\t");
+  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
- 
+float gx = 0;
+float gy = 0;
+float gz = 0;
+float gv = 0;
+float gda = 0;
+float gm = 0;
+float threshold = 10;
 void setup() {
   Serial.begin(115200);
   Wire.begin();
@@ -39,7 +45,7 @@ void setup() {
     while (1) delay(10);  // halt if sensor not found
   }
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
@@ -60,24 +66,40 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
+
 }
 
  
 void loop() {
  sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-
-  if (abs(a.acceleration.x) > 19.8 || abs(a.acceleration.y) > 9.8) {
+  gv = gx*gx + gy*gy + gz*gz;
+  Serial.print("x:");
+  Serial.print(a.acceleration.x);
+  Serial.print(" y:");
+  Serial.print(a.acceleration.y);
+  Serial.print(" z:");
+  Serial.print(a.acceleration.z);
+  gm = pow(a.acceleration.x,2) + pow(a.acceleration.y,2) + pow(a.acceleration.z,2);
+  gda = a.acceleration.x*gx + a.acceleration.y*gy + a.acceleration.z*gz;
+  if (gm > 9.8*9.8-threshold && gm < 9.8*9.8+threshold) {
+    gx = a.acceleration.x;
+    gy = a.acceleration.y;
+    gz = a.acceleration.z;
+  }
+  Serial.print(" gda:");
+  Serial.println(gda);
+  if (gda/gv > 2) {
     // Set values to send
     MIDIData.id = 10;
     MIDIData.note = 56;
-    MIDIData.velocity = abs(map(a.acceleration.x,0,64,1,127));
+    MIDIData.velocity = abs(map(gda/gv,0,64,1,127));
 
     // Send message via ESP-NOW
     esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &MIDIData, sizeof(MIDIData));
     
     if (result == ESP_OK) {
-      Serial.println("Sent with success");
+      //Serial.println("Sent with success");
     }
     else {
       Serial.println("Error sending the data");
